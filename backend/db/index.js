a1995234b481d8193891bcd71fa55b44053b8119
SaiRuthwik22 <160124679+SaiRuthwik22@users.noreply.github.com>
2024-08-19 10:28:49 +0530

    Update index.js

--- backend/db/index.js
+++ backend/db/index.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose"
 
 async function connectDB(req,res){
     try{
-        const result = await mongoose.connect('mongodb+srv://ruthwik242:12123You@cluster0.sbcd5.mongodb.net/demo');
+        const result = await mongoose.connect('mongodb+srv://yourusername:yourpassword@cluster0.sbcd5.mongodb.net/demo');
     }
     catch{
         console.log("error while conecting to mongodb database")
